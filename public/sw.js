8d2617e904ac11bd9ec44349328bbfacd2b1a666
jpreinoso <juan.pablo.reinoso@gmail.com>
2026-05-24 17:13:33 -0400

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'piedras-vivas-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // We only cache GET requests
+  if (e.request.method !== 'GET') return;
+  
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        // If successful, clone and save in cache
+        if (res.status === 200) {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, resClone);
+          });
+        }
+        return res;
+      })
+      .catch(() => {
+        return caches.match(e.request);
+      })
+  );
+});
